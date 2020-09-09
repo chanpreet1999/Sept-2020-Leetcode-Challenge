eebc8ffba4ebfaa9e508661085ab5bc334023f14
Chanpreet Singh <chanpreet1999@gmail.com>
2020-09-10 00:05:49 +0530

    Day9 Solution2

--- Day9/Solution.java
+++ Day9/Solution.java
@@ -36,4 +36,23 @@ class Solution {
         
         return 0;
     }
+
+    //shorter way same time more space
+    public int compareVersion2(String version1, String version2) {
+        String v1[] = version1.split("\\.");
+        String v2[] = version2.split("\\.");
+        
+        int maxLength = Math.max(v1.length, v2.length);
+        for(int i = 0; i < maxLength; i++)
+        {
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i < v2.length ? Integer.parseInt(v2[i]): 0;
+            if(num1 > num2)
+                return 1;
+            else if(num2 > num1)
+                return -1;           
+        }
+        return 0;        
+        
+    }
 }
