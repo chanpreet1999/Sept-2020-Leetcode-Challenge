5f68edcd48d6c1133b00c805b47c2da6d6e268f8
Chanpreet Singh <chanpreet1999@gmail.com>
2020-11-10 08:11:39 +0530

    Error Correction

--- Day7/Solution.java
+++ Day7/Solution.java
@@ -1,33 +1,32 @@
 class Solution {
-    public int largestOverlap(int[][] a, int[][] b) {
-        int ans = 0;
-        int rows = a.length;
-        int cols = a[0].length;
-        for(int r = -rows + 1; r < rows; r++)
-        {
-            for(int c = -cols+1; c < cols; c++)
-            {
-                ans =Math.max( ans, oneOverlap( a, b, r, c ) );
-            }
-        }
-        return ans;
-    }
-    
-    int oneOverlap(int a[][], int b[][], int ro, int co)
-    {
-        int count = 0;
-        int len = a.length;
-        int ans = 0;
+    public boolean wordPattern(String pattern, String str) {
+        String words[] = str.split(" ");
+        int len = str.length();
+        
+        if(len != pattern.length())
+            return false;
+        
+        HashMap<Character, String> hm = new HashMap<>();
+        
         for(int i = 0; i < len; i++)
         {
-            for(int j = 0; j < len; j++)
+            Character ch = pattern.charAt(i);
+            if( !hm.containsKey( ch )  )
             {
-                if(  i + ro < 0 || i + ro >= len || j + co < 0 || j+co >= len )
-                    continue;
-                if( a[i][j] + b[i + ro][j + co] > 1 )
-                    ans++;
+                if( hm.containsValue( words[i] ) )
+                    return false;
+                else
+                {
+                    hm.put( ch, words[i] );
+                }
+            }
+            else
+            {
+                String val = hm.get(ch);
+                if( !val.equals( words[i] ) )
+                    return false;
             }
         }
-        return ans;
+        return true;
     }
 }
